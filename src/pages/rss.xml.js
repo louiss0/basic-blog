f52ceda692637da4b23957296021421e0ea8ceed
Anders Kaseorg <andersk@mit.edu>
2022-12-17 15:27:12 -0800

    Generate RSS with getCollection() API

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,19 @@
 import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 
-const posts = Object.values(
-  import.meta.glob("../content/blog/**/*.{md,mdx}", { eager: true })
-);
+export const get = async () => {
+  const posts = await getCollection("blog");
 
-export const get = () =>
-  rss({
+  return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
     items: posts.map((post) => ({
-      title: post.frontmatter.title,
-      description: post.frontmatter.description,
-      pubDate: post.frontmatter.pubDate,
-      link: post.url,
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: post.slug,
     })),
   });
+};
